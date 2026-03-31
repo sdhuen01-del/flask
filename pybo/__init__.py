407f8f6b7269f8d1d323b5acc0096278048876f1
sdhuen01-del <sdhuen01@gmail.com>
2026-03-31 13:06:10 +0900

    2026033101

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    #orm
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+
 
 
 #블루 프린트 등록
